ec63a78620d126980e930dea59e0a281a8f26225
Brad Cypert <brad.cypert@gmail.com>
2015-10-27 21:28:15 -0700

    project.clj for release on Clojars.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject clojchimp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject clojchimp "1.0.1"
+  :description "A small mailchimp client written in Clojure"
+  :url "http://github.com/bradcypert/clojchimp"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "2.0.0"]
                  [clj-http-fake "1.0.1"]
